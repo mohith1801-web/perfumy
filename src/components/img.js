6e08648df606cd3d86098c626a73d304c1724496
mohith1801-web <mohithraja1801@gmail.com>
2026-03-28 23:57:32 +0530

    updated

--- src/components/img.js
+++ src/components/img.js
@@ -1,20 +1,20 @@
-import one from "../assects/imgs/one.jpg"
+import one from "../assects/imgs/one.jpg" 
 import two from "../assects/imgs/two.jpg"
 import three from "../assects/imgs/three.jpg"
 function Images () {
     return (
 <div className="products">
             <div className="box">
-                        <img src={one}></img>
+                        <img src={one} alt="img"></img>
                         <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>    
             </div>
             
             <div className="box">
-                <img src={two}></img>
+                <img src={two} alt="img"></img>
                 <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p> 
             </div>
             <div className="box">
-                <img src={three}></img>
+                <img src={three} alt="img"></img>
                 <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>
             </div>            
         </div>
